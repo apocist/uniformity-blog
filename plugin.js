3c07fe006e2dda6c6d6fe11e8f233dec8413285c
Apocist <apocist@yahoo.com>
2016-03-10 20:27:45 -0800

    plugin progress

--- plugin.js
+++ plugin.js
@@ -1,24 +1,26 @@
-module.exports =  {
-	name: 'Blog',
-	description: 'Tests at install blog function',
-	models: {
-		blog: __dirname+'/app/models/routable/blog.routable.server.model.js'
-	},
-	controllers: {
-		blog: __dirname+'/app/controllers/routable/blog.routable.server.controller.js'
-	},
-	routes: {},
-	views: {
-		blog: __dirname+'/app/views'
-	},
-	loadOrder:{
-		'model.postRoutable': [{
-			order: 2000,
-			item: models.blog
-		}],
-		'view.postSite': [{
-			order: 2000,
-			item: views.blog
-		}]
-	}
+module.exports = function(callback) {
+	callback({
+		name: 'Blog',
+		description: 'Tests at install blog function',
+		models: {
+			blog: __dirname+'/app/models/routable/blog.routable.server.model.js'
+		},
+		controllers: {
+			blog: __dirname+'/app/controllers/routable/blog.routable.server.controller.js'
+		},
+		routes: {},
+		views: {
+			blog: __dirname+'/app/views'
+		},
+		loadOrder:{
+			'model.postRoutable': [{
+				order: 2000,
+				item: __dirname+'/app/models/routable/blog.routable.server.model.js'
+			}],
+			'view.postSite': [{
+				order: 2000,
+				item: __dirname+'/app/views'
+			}]
+		}
+	});
 };
